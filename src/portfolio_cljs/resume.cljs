6a8e3ad60e01901830f52ea6e305c2b2edc2cae9
Andrew Cotter <cotter.andrew.p@gmail.com>
2018-11-10 09:46:11 -0500

    resume content

--- src/portfolio_cljs/resume.cljs
+++ src/portfolio_cljs/resume.cljs
@@ -42,95 +42,113 @@
     [:section.section
         [:div.tile.is-ancestor
             [:div.tile.is-vertical.is-7
-                [:article.tile.is-parent.
-                    [:h1.title.is-child "Professional Experience"
-                        [:br]
-                        [:br]
-                        [:article.tile.is-child.box
-                            [:p.title.is-4 "Local Projects, Creative Technologist"]
-                            [:p.subtitle "Spring 2018 - Present"
-                                [:div.content.has-text-weight-light.is-7 short-ipsum]]]
-                        [:br]
-                        [:div.tile.is-child.box
-                            [:p.title.is-4 "Red Paper Heart, Code Intern"]
-                            [:p.subtitle "Summer 2017"
-                                [:div.content.has-text-weight-light.is-7 short-ipsum]]]
-                        [:br]
-                        [:div.tile.is-child.box
-                            [:p.title.is-4 "Cipher Prime, Intern"]
-                            [:p.subtitle "Fall 2015 - Summer 2016"
-                                [:div.content.has-text-weight-light.is-7 short-ipsum]]]
-                        [:br]
-                        [:div.tile.is-child.box
-                            [:p.title.is-4 "Night Kitchen, Interaction Developer"]
-                            [:p.subtitle "Fall 2014 - Summer 2016"
-                                [:div.content.has-text-weight-light.is-7 short-ipsum]]]
-                        [:br]
-                        [:br]]]
+                [:article.tile.is-parent.is-vertical
+                    [:h1.title.is-child "Professional Experience"]
+                    [:article.tile.is-child.box
+                        [:p.title.is-4 "Local Projects, Creative Technologist"]
+                        [:p.subtitle "Spring 2018 - Present"]
+                        [:p.is-size-6.has-text-weight-light short-ipsum]]
+                    [:div.tile.is-child.box
+                        [:p.title.is-4 "Red Paper Heart, Code Intern"]
+                        [:p.subtitle "Summer 2017"]
+                        [:p.is-size-6.has-text-weight-light.is-7 "Worked in tangent with the design team to
+                        program/build interactive installations and
+                        experiences for commercial clients using
+                        openFrameworks, Cinder, and Arduino."]]
+                    [:div.tile.is-child.box
+                        [:p.title.is-4 "Cipher Prime, Intern"]
+                        [:p.subtitle "Fall 2015 - Summer 2016"]
+                        [:p.is-size-6.has-text-weight-light.is-7 "Assisting with design/development of video
+                        games and interactive media in various
+                        frameworks such as Unity, Cinder, and P5.js."]]
+                    [:div.tile.is-child.box
+                        [:p.title.is-4 "Night Kitchen, Interaction Developer"]
+                        [:p.subtitle "Fall 2014 - Summer 2016"]
+                        [:p.is-size-6.has-text-weight-light.is-7 "Lead design/development research by
+                        exploring new tools for the web and
+                        interactive installations and demonstrating
+                        implementations to the rest of the team."]]
+                    [:br]]
                 [:div.tile.is-parent
                     [:div.tile.is-child
-                        [:p.title "Teaching Experience"
-                            [:br]
-                            [:br]
-                            [:article.tile.is-child.box
-                                [:p.title.is-4 "Instructor, Creativity + Computation"]
-                                [:p.subtitle "Parsons, Fall 2018"
-                                    [:div.content.has-text-weight-light.is-7 short-ipsum]]]
-                            [:br]
-                            [:article.tile.is-child.box
-                                [:p.title.is-4 "Teaching Assistant, Immersive Storytelling"]
-                                [:p.subtitle "Parsons, Fall 2017"
-                                    [:div.content.has-text-weight-light.is-7 short-ipsum]]]
-                            [:br]
-                            [:article.tile.is-child.box
-                                [:p.title.is-4 "Code Instructor, D+T  Bootcamp"]
-                                [:p.subtitle "Parsons, Summer 2017"
-                                    [:div.content.has-text-weight-light.is-7 short-ipsum]]]
-                            [:br]]]]]
+                        [:p.title "Teaching Experience"]
+                        [:div.content.box
+                            [:p.title.is-4 "Instructor, Creativity + Computation"]
+                            [:p.subtitle "Parsons, Fall 2018"]
+                            [:div.content.has-text-weight-light.is-7 short-ipsum]]
+                        [:div.content.box
+                            [:p.title.is-4 "Teaching Assistant, Immersive Storytelling"]
+                            [:p.subtitle "Parsons, Fall 2017"]
+                            [:div.content.has-text-weight-light.is-7 short-ipsum]]
+                        [:div.content.box
+                            [:p.title.is-4 "Code Instructor, D+T  Bootcamp"]
+                            [:p.subtitle "Parsons, Summer 2017"]
+                            [:div.content.has-text-weight-light.is-7 short-ipsum]]]]] 
             [:div.tile.is-parent.is-vertical.is-5
-                    [:p.title "Skills / Tools"
-                        [:br]
-                        [:br]
-                        [:div.tile.box
-                            [:p.title.is-5 "Adept"
-                                [:dl.subtitle.is-6
-                                    [:li "Electron"]
-                                    [:li "WebGL (Pixi.js, Three.js, ReGL, p5.js)"]
-                                    [:li "Front End Web (HTML/SCSS/JS)"]
-                                    [:li "openFrameworks (C++)"]
-                                    [:li "Arduino / Physical Computing"]]]]
-                        [:div.tile.box
-                            [:p.title.is-5 "Intermediate"
-                                [:dl.subtitle.is-6
-                                    [:li "Unity / C#"]
-                                    [:li "Node.js"]
-                                    [:li "Fragment Shaders"]
-                                    [:li "Clojure(Script)/(CLR)"]]]]
-                        [:div.tile.box
-                            [:p.title.is-5 "Familiar"
-                                [:dl.subtitle.is-6
-                                    [:li "Python"]
-                                    [:li "Rust"]
-                                    [:li "Vert Shaders"]
-                                    [:li "Jupyter Notebooks"]]]]
-                        [:div.tile.box
-                            [:p.title.is-5 "Learning"
-                                [:dl.subtitle.is-6
-                                    [:li "Swift/iOS"]
-                                    [:li "Design Patterns"]
-                                    [:li "Compiler Design and Implementation"]]]]
-                        [:br]]
-                    [:p.title "Education / Honors"
-                        [:br]
-                        [:br]
-                        [:div.tile.is-vertical.content.box
-                            [:p.title.is-5 "MFA Design + Technology"]
-                            [:p.subtitle.is-5 "Parsons, Class of 2018"
-                                [:div.content.has-text-weight-light "President of Dorkshop 2017-2018"]]]
-                        [:div.tile.is-vertical.content.box
-                            [:p.title.is-5 "BS Industrial Design"]
-                            [:p.subtitle.is-5 "University of the Arts, Class of 2014"
-                                [:div.content.has-text-weight-light "President of Literary Society 2012-2014"]]]
-                        [:div.tile.is-vertical.content.box
-                            [:p.title.is-5 "Attained the rank of Eagle Scout"]
-                            [:p.subtitle.is-5 "BSA Toop 139, 2010"]]]]]])
+                    ; [:content..is-child.tile.box
+                    [:p.title "Core Competencies"]
+                    [:div.content.box
+                        [:p.title.is-5 "Creative Coding"]
+                        [:p.is-size-6.has-text-weight-light.is-7 "I approach writing
+                        code like writing
+                        poetry, focusing on
+                        the mechanics, the
+                        demystification of
+                        tools, and hacking
+                        the conventions
+                        of art-making with
+                        computation."]]
+                    [:div.content.box
+                        [:p.title.is-5 "Design Research"]
+                        [:p.is-size-6.has-text-weight-light.is-7 "The first step to
+                        solving a problem
+                        is understanding
+                        it. By interviewing
+                        end-users and testing
+                        prototypes, I can better
+                        understand what the
+                        solution is."]]
+                    [:br]
+                    [:p.title "Skills + Tools"]
+                    [:div.tile.is-vertical.box
+                        [:p.title.is-5 "Adept"]
+                        [:dl.is-6
+                            [:li "Electron"]
+                            [:li "WebGL (Pixi.js, Three.js, ReGL, p5.js)"]
+                            [:li "Front End Web (HTML/SCSS/JS)"]
+                            [:li "openFrameworks (C++)"]
+                            [:li "Arduino / Physical Computing"]
+                            [:li "Adobe CC Workflow (esp. Ps, Ai, Id, Lr)"]]]
+                    [:div.tile.is-vertical.box
+                        [:p.title.is-5 "Intermediate"]
+                        [:dl.is-6
+                            [:li "Node.js"]
+                            [:li "Fragment Shaders"]
+                            [:li "Unity / C#"]
+                            [:li "Clojure(Script)/(CLR)"]]]
+                    [:div.tile.is-vertical.box
+                        [:p.title.is-5 "Familiar"]
+                        [:dl.is-6
+                            [:li "Python"]
+                            [:li "Rust"]
+                            [:li "Vert Shaders"]
+                            [:li "Jupyter Notebooks"]]]
+                    [:div.tile.is-vertical.box
+                        [:p.title.is-5 "Learning"]
+                        [:dl.is-6
+                            [:li "Swift/iOS"]
+                            [:li "Design Patterns"]
+                            [:li "Compiler Design and Implementation"]]]
+                    [:br]
+                    [:p.title "Education + Honors"]
+                    [:div.tile.is-vertical.content.box
+                        [:p.title.is-5 "MFA Design + Technology"]
+                        [:p.subtitle.is-5 "Parsons, Class of 2018"
+                            [:p.is-size-6.has-text-weight-light "President of Dorkshop 2017-2018"]]]
+                    [:div.tile.is-vertical.content.box
+                        [:p.title.is-5 "BS Industrial Design"]
+                        [:p.subtitle.is-5 "University of the Arts, Class of 2014"
+                            [:p.is-size-6.has-text-weight-light "President of Literary Society 2012-2014"]]]
+                    [:div.tile.is-vertical.content.box
+                        [:p.title.is-5 "Attained the rank of Eagle Scout"]
+                        [:p.subtitle.is-5 "BSA Toop 139, 2010"]]]]])
